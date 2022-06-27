c172e7b3312f8be4ea27b118d206d542ac9b8524
guillermoandrae <guillermoandraefisher@gmail.com>
2022-06-27 11:16:52 -0400

    Getting env variables and what not.

--- bin/index.php
+++ bin/index.php
@@ -6,6 +6,9 @@ use GuzzleHttp\Client;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+define('GIPHY_API_URL', getenv('GIPHY_API_URL', true) ?: getenv('GIPHY_API_URL'));
+define('GIPHY_API_KEY', getenv('GIPHY_API_KEY', true) ?: getenv('GIPHY_API_KEY'));
+
 return function (array $event) {
     $client = new Client();
     $giphy = new Giphy();
